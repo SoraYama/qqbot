e5f8cd48105a674a3fe7a4092108758c55757f63
SoraYama <sorayamahou@gmail.com>
2020-03-25 18:24:48 +0800

    fix: catch error to fix text

--- app/modules/kancolle/mini/store/order.ts
+++ app/modules/kancolle/mini/store/order.ts
@@ -157,7 +157,7 @@ class Order {
     _([this.toTrade, this.wanted]).each((item) => {
       if (_(item).some((i) => !_.isInteger(i))) {
         logger.error(this.orderType, this.toTrade, this.wanted);
-        throw new Error('输入非法');
+        throw new Error('资源或舰娘ID输入非法, 请重新创建订单');
       }
     });
     this.handleTrading();
@@ -167,18 +167,34 @@ class Order {
     const config = tradeConfigs[this.orderType];
     const { toTrade, wanted } = config;
     if (toTrade === 'ship') {
-      _.each(this.toTrade, (id) => this.seller?.dropShip(id));
+      try {
+        _.each(this.toTrade, (id) => this.seller?.dropShip(id));
+      } catch (e) {
+        throw new Error('订单发起者目前没有该舰娘或该舰为秘书舰, 不能交易');
+      }
       _.each(this.toTrade, (id) => this.buyer?.addShip(id));
     } else if (toTrade === 'resource') {
-      this.seller?.addResource(this.toTrade.map((r) => -r));
+      try {
+        this.seller?.addResource(this.toTrade.map((r) => -r));
+      } catch (e) {
+        throw new Error('订单发起者资源不足');
+      }
       this.buyer?.addResource(this.toTrade);
     }
     if (wanted === 'ship') {
       _.each(this.wanted, (id) => this.seller?.addShip(id));
-      _.each(this.wanted, (id) => this.buyer?.dropShip(id));
+      try {
+        _.each(this.wanted, (id) => this.buyer?.dropShip(id));
+      } catch (e) {
+        throw new Error('你目前没有该舰娘或该舰为秘书舰, 不能交易');
+      }
     } else if (wanted === 'resource') {
       this.seller?.addResource(this.wanted);
-      this.buyer?.addResource(this.wanted.map((r) => -r));
+      try {
+        this.buyer?.addResource(this.wanted.map((r) => -r));
+      } catch (e) {
+        throw new Error('你目前资源不足');
+      }
     }
   }
 
